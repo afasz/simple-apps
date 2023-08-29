9ee83909154253f50573cd3205a74b4840ac31c4
afasz <afasz1123@gmail.com>
2023-08-29 10:33:33 +0700

    Tambah route apps fauzan

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.get('/app2', (req, res) => {
   res.send('Hello this App 2!')
 });
 
+app.get('/fauzan', (req, res) => {
+  res.send('Coba create apss baru')
+});
+
 app.get('/users', (req, res, next) => {
   const sql = "SELECT * FROM tb_data ORDER BY id desc"
   connection.query(sql,(error, fields) => {
